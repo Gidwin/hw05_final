2fd8f8c72c0e3cb76609b627421b382b62b9a0ab
Лежакин Никита <gidwin@mail.ru>
2023-01-18 20:45:39 +0300

    добавлен README.md

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.gidwin.pythonanywhere.com',
+    'gidwin.pythonanywhere.com',
 ]
 
 
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
